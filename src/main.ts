6d9797416e60a567cc2369f896504eb7e562b3af
beni69 <73666440+beni69@users.noreply.github.com>
2023-08-09 00:09:14 +0200

    update to new roland

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,11 @@ import { Robot } from "@kareszklub/roblib-client";
 
 (async () => {
     const IP = prompt("Robot IP", "localhost")!;
-    const robot = await Robot.connect(`ws://${IP}:1111/ws`, true);
+    const robot = await Robot.connect(IP);
 
     const DELAY = 50;
 
-    let speed = 50,
+    let speed = .5,
         moving = false;
 
     const speedSlider = document.getElementById("speed") as HTMLInputElement,
@@ -19,28 +19,28 @@ import { Robot } from "@kareszklub/roblib-client";
                 keys: ["arrowup", "w", "k"], // keyboard shortcuts
                 // function to run
                 onPress: () => {
-                    robot.move(speed, speed);
+                    robot.drive(speed, speed);
                 },
             },
             down: {
                 element: document.getElementById("down"),
                 keys: ["arrowdown", "s", "j"],
                 onPress: () => {
-                    robot.move(speed * -1, speed * -1);
+                    robot.drive(speed * -1, speed * -1);
                 },
             },
             left: {
                 element: document.getElementById("left"),
                 keys: ["arrowleft", "a", "h"],
                 onPress: () => {
-                    robot.move(speed * -1, speed);
+                    robot.drive(speed * -1, speed);
                 },
             },
             right: {
                 element: document.getElementById("right"),
                 keys: ["arrowright", "d", "l"],
                 onPress: () => {
-                    robot.move(speed, speed * -1);
+                    robot.drive(speed, speed * -1);
                 },
             },
             break: {
@@ -55,13 +55,11 @@ import { Robot } from "@kareszklub/roblib-client";
                 keys: ["q"],
                 color: "turquoise", // button background color
                 onPress: () => {
-                    const led = {
-                        r: !!Math.round(Math.random()),
-                        g: !!Math.round(Math.random()),
-                        b: !!Math.round(Math.random()),
-                    };
-                    console.log(led);
-                    robot.led(led);
+                    const r = !!Math.round(Math.random()),
+                        g = !!Math.round(Math.random()),
+                        b = !!Math.round(Math.random());
+                    console.log({ r, g, b });
+                    robot.led(r, g, b);
                 },
             },
             horn: {
@@ -92,7 +90,7 @@ import { Robot } from "@kareszklub/roblib-client";
             a.keys.includes(event.key.toLowerCase())
         );
 
-        // dont move it already moving
+        // dont drive it already moving
         if (!action || (moving && action.keys[0] !== " ")) return;
         moving = true;
 
@@ -151,8 +149,8 @@ import { Robot } from "@kareszklub/roblib-client";
 
     // set the slider when the speed changes
     function setSpeed(s: number) {
-        speed = s;
-        speedSlider.value = speed as any;
+        speed = s / 100;
+        speedSlider.value = s as any;
     }
     // change speed with the mouse wheel
     document.onwheel = event => {
